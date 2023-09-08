b1fefe54d346b293c4a60a263ca72574832afa75
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-08 12:13:44 -0700

    Print possible combinations of single digits.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,15 +6,17 @@
  */
 int main(void)
 {
-	int num = 0;
+	int num = 0;	
 	while (num < 10)
 	{
 		putchar(48 + num);
-		putchar(',');
-		putchar(' ');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		num++;
 	}
 	putchar('\n');
 	return (0);
 }
-  
