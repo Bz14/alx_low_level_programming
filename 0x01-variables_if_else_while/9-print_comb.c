1ba5db2bdb8cbd3a65c75d195f0ffa831e662f9c
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-08 12:06:19 -0700

    Print possibel combinations of single digits.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+	while (num < 10)
+	{
+		putchar(48 + num);
+		putchar(',');
+		putchar(' ');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
+  
