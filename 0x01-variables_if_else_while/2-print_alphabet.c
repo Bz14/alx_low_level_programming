98b0e9c734b9b25646273dc6f515425782bb7260
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-08 08:23:58 -0700

    Print lowercase letters using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Empty Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char letter='a';
+	while (letter <= 'z')
+	{
+	  putchar(letter);
+	  letter++;
+	}
+	return (0);
+}
