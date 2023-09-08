1873a022d852997f524b1f5565917e4349cf110b
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-08 08:57:04 -0700

    Print both uppercases and lowecases.

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,15 @@ int main(void)
 {
 char lower = 'a', upper = 'A';
 while (lower <= 'z')
+{
 putchar(lower);
+lower++;
+}
 while (upper <= 'Z')
+{
 putchar(upper);
+upper++;
+}
 putchar('\n');
 return (0);
 }
