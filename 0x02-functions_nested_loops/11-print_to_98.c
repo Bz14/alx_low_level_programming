814890b64574d79c170f7913bfc63f0d686712a3
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-13 07:44:39 -0700

    Print numbers upto 98.

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_to_98 - Print upto 98
+ *
+ * @n: an integer number
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; ++i)
+		{
+			_putchar(48 + n);
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; --i)
+		{
+			_putchar(48 + n);
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
