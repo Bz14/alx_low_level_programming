028b629ff5d9d4ae9b54b8513b60f25e67bd6046
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-13 04:05:04 -0700

    Print loercases in two dimensions.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,22 @@
 #include "main.h"
+/**
+ * print_alphabet_x10 - print alphabets in two dimensions
+ *
+ * Return: Always 0.
+ */
 void print_alphabet_x10(void)
 {
 	char lower;
-        int i = 0;
-        while (i < 10)
-        {
+	int i = 0;
+	while (i < 10)
+	{
 		lower = 'a';
-        while (lower <= 'z')
+	while (lower <= 'z')
 	{
 		_putchar(lower);
 		lower++;
 	}
 	i++;
-        _putchar('\n');
+	_putchar('\n');
 	}
 }
