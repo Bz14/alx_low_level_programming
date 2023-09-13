020996076a90fdde59876d08293513ef33cfc8b9
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-13 04:33:54 -0700

    Determine whether lowercase or not.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - check whether lowercase or not.
+ *
+ * @parameterc : a single character
+ *
+ * Return: Always return 1 or 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
