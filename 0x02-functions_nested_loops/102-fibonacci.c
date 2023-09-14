e6202603ced516e395ee26d18c20b512ce206464
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-14 10:23:36 -0700

    The first fiboncchi numbers.

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int num;
+	int count = 2;
+
+	printf("%d, %d, ", a, b);
+	while (count <= 50)
+	{
+		num = a + b;
+		printf("%d", num);
+		a = b;
+		b = num;
+		if (count != 50)
+		{
+			printf(", ");
+		}
+		count++;
+	}
+	return (0);
+}
