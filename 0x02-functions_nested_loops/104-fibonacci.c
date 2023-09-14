d6deff0bcd5ad32abda8e6dfca3ea545c9e1c4f8
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-14 13:49:17 -0700

    Print the first 98 fibonacci.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <limits.h>
 /**
  * main - Entry point
  *
@@ -7,38 +6,39 @@
  */
 int main(void)
 {
-  unsigned long a = 0, b = 1, num;
-  unsigned long a_half1, a_half2, b_half1, b_half2;
-  unsigned long firsthalf, secondhalf;
-  int count;
+	unsigned long a = 0, b = 1, num;
+	unsigned long a_half1, a_half2, b_half1, b_half2;
+	unsigned long firsthalf, secondhalf;
+	int count;
 
-  for (count = 0; count < 92; count++)
-    {
-      num = a + b;
-      printf("%lu, ", num);
-      a = b;
-      b = num;
-    }
-  a_half1 = a / 10000000000;
-  b_half1 = b / 10000000000;
-  a_half2 = a % 10000000000;
-  b_half2 = b % 10000000000;
-  for (count = 93; count < 99; count++)
-    {
-      firsthalf = a_half1 + b_half1;
-      secondhalf = a_half2 + b_half2;
-      if (a_half2 + b_half2 > 9999999999)
-	{ firsthalf += 1;
-	  secondhalf %= 10000000000;
+	for (count = 0; count < 92; count++)
+	{
+		num = a + b;
+		printf("%lu, ", num);
+		a = b;
+		b = num;
 	}
-      printf("%lu%lu", firsthalf, secondhalf);
-      if (count != 98)
-	printf(", ");
-      a_half1 = b_half1;
-      a_half2 = b_half2;
-      b_half1 = firsthalf;
-      b_half2 = secondhalf;
-    }
-  putchar('\n');
-  return (0);
+	a_half1 = a / 10000000000;
+	b_half1 = b / 10000000000;
+	a_half2 = a % 10000000000;
+	b_half2 = b % 10000000000;
+	for (count = 93; count < 99; count++)
+	{
+		firsthalf = a_half1 + b_half1;
+		secondhalf = a_half2 + b_half2;
+		if (a_half2 + b_half2 > 9999999999)
+		{
+			firsthalf += 1;
+			secondhalf %= 10000000000;
+		}
+		printf("%lu%lu", firsthalf, secondhalf);
+		if (count != 98)
+			printf(", ");
+		a_half1 = b_half1;
+		a_half2 = b_half2;
+		b_half1 = firsthalf;
+		b_half2 = secondhalf;
+	}
+	putchar('\n');
+	return (0);
 }
