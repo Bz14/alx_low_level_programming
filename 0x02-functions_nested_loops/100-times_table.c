d4bd78fed41d5a1eabdf875350a0d48bbc8e4ecb
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-14 12:16:56 -0700

    Print times table.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,8 +29,24 @@ void print_times_table(int n)
 		  _putchar((((i * j) / 10) % 10) + 48);
 		  _putchar(((i * j) % 10) + 48);
 		}
-	      _putchar(',');
-	      _putchar(' ');
+	      if ((i * (j + 1) > 10) && j != n)
+		{
+		  _putchar(',');
+		  _putchar(' ');
+		  _putchar(' ');
+		}
+	      else if ((i * (j + 1) <= 10) && j != n)
+		{
+		  _putchar(',');
+		  _putchar(' ');
+		  _putchar(' ');
+		  _putchar(' ');
+		}
+	      else if ((i * (j + 1) >= 100) && j != n)
+		{
+		  _putchar(',');
+		  _putchar(' ');
+		}
 	    }
 	  _putchar('\n');
 	}
