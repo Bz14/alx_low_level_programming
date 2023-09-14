e6111890b63fa2d2adbbcd52c2df562201240c43
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-14 06:28:03 -0700

    Print upto 98.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,18 +12,18 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; ++i)
+		for (i = n; i >= 98; ++i)
 		{
-			if ( n > 9)
+			if (n > 9)
 			{
-				_putchar((n / 10) + 48);
-				_putchar((n % 10) + 48);
+				_putchar((i / 10) + 48);
+				_putchar((i % 10) + 48);
 			}
 			else
 			{
-				_putchar(n + 48);
+				_putchar(i + 48);
 			}
-			if (n != 98)
+			if (i != 98)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -34,18 +34,18 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; --i)
 		{
-			if ( n >= 99)
+			if (i >= 99)
 			{
-				_putchar((n / 10) + 48);
-				_putchar((n % 10) + 48);
+				_putchar((i / 10) + 48);
+				_putchar((i % 10) + 48);
 			}
 			else
 			  {
-			    	_putchar((n / 100) + 48);
-				_putchar(((n / 10) % 10) + 48);
-				_putchar((n % 10) + 48);
+			    	_putchar((i / 100) + 48);
+				_putchar(((i / 10) % 10) + 48);
+				_putchar((i % 10) + 48);
 			}
-			if (n != 98)
+			if (i != 98)
 			{
 				_putchar(',');
 				_putchar(' ');
