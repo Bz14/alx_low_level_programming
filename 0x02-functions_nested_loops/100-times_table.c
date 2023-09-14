ba7a19b5704d13431847eba999b8a577f66bf1fe
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-14 12:43:41 -0700

    Print times table.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,49 +6,47 @@
  */
 void print_times_table(int n)
 {
-  int i, j;
+	int i, j;
 
-  if (n > 0 && n <= 15)
-    {
-      for (i = 0; i <= n; ++i)
+	if (n >= 0 && n <= 15)
 	{
-	  for (j = 0; j <= n; ++j)
-	    {
-	      if (i * j < 10)
+		for (i = 0; i <= n; ++i)
 		{
-		  _putchar((i * j) + 48);
+			for (j = 0; j <= n; ++j)
+			{
+				if (i * j < 10)
+					_putchar((i * j) + 48);
+				else if (i * j < 100)
+				{
+					_putchar(((i * j) / 10) + 48);
+					_putchar(((i * j) % 10) + 48);
+				}
+				else
+				{
+					_putchar(((i * j) / 100) + 48);
+					_putchar((((i * j) / 10) % 10) + 48);
+					_putchar(((i * j) % 10) + 48);
+				}
+				if (((i * (j + 1) >= 10) && (i * (j + 1) < 100)) && j != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if ((i * (j + 1) < 10) && j != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if ((i * (j + 1) >= 100) && j != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
 		}
-	      else if (i * j < 100)
-		{
-		  _putchar(((i * j) / 10) + 48);
-		  _putchar(((i * j) % 10) + 48);
-		}
-	      else
-		{
-		  _putchar(((i * j) / 100) + 48);
-		  _putchar((((i * j) / 10) % 10) + 48);
-		  _putchar(((i * j) % 10) + 48);
-		}
-	      if (((i * (j + 1) >= 10) && (i * (j + 1) < 100)) && j != n)
-		{
-		  _putchar(',');
-		  _putchar(' ');
-		  _putchar(' ');
-		}
-	      else if ((i * (j + 1) < 10) && j != n)
-		{
-		  _putchar(',');
-		  _putchar(' ');
-		  _putchar(' ');
-		  _putchar(' ');
-		}
-	      else if ((i * (j + 1) >= 100) && j != n)
-		{
-		  _putchar(',');
-		  _putchar(' ');
-		}
-	    }
-	  _putchar('\n');
 	}
-    }
 }
