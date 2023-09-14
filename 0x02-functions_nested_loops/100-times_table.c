c3790e06a3b895c03762c88c7a98af73c889121d
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-14 11:57:04 -0700

    Print times table.

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include <main.h>
+/**
+ * print_times_table - Print times table
+ *
+ * @n: an integer
+ */
+void print_times_table(int n)
+{
+  int i, j;
+
+  if (n > 0 && n <= 15)
+    {
+      for (i = 0; i <= n; ++i)
+	{
+	  for (j = 0; j <= n; ++j)
+	    {
+	      if (i * j < 10)
+		{
+		  _putchar((i * j) + 48);
+		}
+	      else if (i * j < 100)
+		{
+		  _putchar(((i * j) / 10) + 48);
+		  _putchar(((i * j) % 10) + 48);
+		}
+	      else
+		{
+		  _putchar(((i * j) / 100) + 48);
+		  _putchar((((i * j) / 10) % 10) + 48);
+		  _putchar(((i * j) % 10) + 48);
+		}
+	      _putchar(',');
+	      _putchar(' ');
+	    }
+	  _putchar('\n');
+	}
+    }
+}
