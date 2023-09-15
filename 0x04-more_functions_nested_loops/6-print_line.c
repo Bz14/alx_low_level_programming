54e1f43517e16665416090ff8f22b7facc74feae
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-15 07:31:31 -0700

    Print a line.

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,11 +10,12 @@ void print_line(int n)
 
 	if (n > 0)
 	{
-		for( i = n; i > 0; i--)
+		for (i = n; i > 0; i--)
 		{
 			_putchar('_');
 		}
 	}
+	else
+		_putchar('\n');
 	_putchar('\n');
 }
-    
