58b23741407d54be74239e02bd7d3e880577df26
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-15 07:25:32 -0700

    Print a line.

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - Print a line
+ *
+ * @n: an integer
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for( i = n; i > 0; i--)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
+    
