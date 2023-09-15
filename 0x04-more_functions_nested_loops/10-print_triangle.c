360734c3a2fbc04fed01b5396492ec48de1d6a73
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-15 12:52:50 -0700

    Print triangle.

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,16 +8,21 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
-	for (i = 1; i <= size; i++)
+	if (size > 0)
 	{
-		for (j = 1; j <= size - i; j++)
+		for (i = 1; i <= size; i++)
 		{
-			_putchar(' ');
-		}
-		for (k = 1; k <= i; k++)
-		{
-			_putchar('#');
-		}
+			for (j = 1; j <= size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 1; k <= i; k++)
+			{
+				_putchar('#');
+			}
 		_putchar('\n');
+		}
 	}
+	else
+		_putchar('\n');
 }
