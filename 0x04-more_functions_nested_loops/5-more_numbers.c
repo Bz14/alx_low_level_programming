4d9549450f3d1c913966d1d4789b3731faad00d2
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-15 12:00:23 -0700

    Print upto 14 in two dimension/

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - Print 1 to 14 in two dimensions
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 14; ++i)
+	{
+		for (j = 0; j <= 14; ++j)
+		{
+			if (j > 9)
+				_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
