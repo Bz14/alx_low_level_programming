015ec1bbef61c305993fa7ebc57c0ab163a122c0
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-18 14:15:40 -0700

    Print a reverse of a string.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "main.h"
 /**
  * print_rev - print a string in reverse
@@ -5,15 +6,9 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, i = 0;
+	int j;
 
-	while (*(s + i)  != '\0')
-	{
-		len += 1;
-		i += 1;
-	}
-	for (i = len; i <= 0; i--)
-	{
-		_putchar(*(s + i));
-	}
+	for (j = strlen(s) - 1; j >= 0; --j)
+		_putchar(*(s + j));
+	_putchar('\n');
 }
