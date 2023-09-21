8d0733ddff8304a2e30ad78917c34b3769bf5a46
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-21 04:09:11 -0700

    Encode a string.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <ctype.h>
 /**
  * leet - encodes a string
  * @n: a string
@@ -8,20 +9,20 @@
 char *leet(char *n)
 {
 	int i, len = strlen(n);
+	char letter[] = {'a', 'e', 'o', 't', 'l'};
+	char number[] = {'4', '3', '0', '7', '1'};
+	int j, let_len = strlen(letter);
 
 	for (i = 0; i < len; ++i)
 	{
-		if (*(n + i) == 'a' || *(n + i) == 'A')
-			*(n + i) = 4 + 48;
-		else if (*(n + i) == 'e' || *(n + i) == 'E')
-			*(n + i) = 3 + 48;
-		else if (*(n + i) == 'o' || *(n + i) == 'O')
-			*(n + i) = 48;
-		else if (*(n + i) == 't' || *(n + i) == 'T')
-			*(n + i) = 7 + 48;
-		else if (*(n + i) == 'l' || *(n + i) == 'L')
-			*(n + i) = 1 + 48;
+		for (j = 0; j <  let_len; j++)
+		{
+			if (*(n + i) == letter[j] || *(n + i) == toupper(letter[j]))
+			{
+				*(n + i) = number[j];
+				break;
+			}
+		}
 	}
-	*(n + i) = '\0';
 	return (n);
 }
