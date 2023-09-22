959b67bab90a3ce2dd04b574cba13a64014b9e3d
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-22 09:26:26 -0700

    Encode by rot13.

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+/**
+ * rot13 - encode a string using rot13
+ * @n: a string
+ * Return: a string
+ */
+char *rot13(char *n)
+{
+	int j, i, len = strlen(n);
+	char enc_1[] = "abcdefghijklmnopqrstuvwxyz";
+	char enc_2[] = "nopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; i < len; ++i)
+		for (j = 0; j < 26; j++)
+			if (*(n + i) == enc_1[j] || *(n + i) == toupper(enc_1[j]))
+			{
+				*(n + i) = enc_2[j];
+				break;
+			}
+	return (n);
+}
