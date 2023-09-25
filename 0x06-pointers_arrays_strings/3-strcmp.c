7f0c81e09de314f0ee9441f1bce63d6b62fe51dc
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-25 12:48:41 -0700

    Compare two strings.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,14 +8,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, len = strlen(s1);
+	int i = 0;
 
-	for (i = 0; i < len; ++i)
+	while (*(s1 + i) != '\0' && *(s2 + i) != '\0')
 	{
-		if (*(s1 + i) < *(s2 + i))
-			return (-15);
-		else if ((*s1 + i) > *(s2 + i))
-			return (15);
+		if (*(s1 + i) != *(s2 + i))
+		{
+			return (*(s1 + i) - *(s2 + i));
+		}
+	i++;
 	}
 	return (0);
 }
