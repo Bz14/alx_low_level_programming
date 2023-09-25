0074387305a0e0ae49d437902d4986473c5b5344
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-25 09:32:53 -0700

    Largest prime factor.

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - largest prime factor
+ * Return: Always Success(0)
+ */
+int main()
+{
+	unsigned long factor = 2, num = 612852475143UL;
+
+	while (factor * factor <= num)
+	{
+		if (num % factor == 0)
+			num /= factor;
+		else
+			factor++;
+	}
+	printf("%lu\n", num);
+	return 0;
+}    
