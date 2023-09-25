7af0c578b7f0cb1855f0112cdd50fa718c5d9b6b
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-25 12:02:49 -0700

    Generate random password.

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,15 +7,22 @@
  */
 int main(void)
 {
-	int i;
-	char pass[8] = {0};
+	int i, sum, n;
+	int pass[100];
 
 	srand(time(NULL));
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 100; i++)
 	{
-		pass[i] = rand() % 26 + 'A';
-		printf("%c", pass[i]);
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		 }
 	}
-	pass[i] = '\0';
 	return (0);
 }
