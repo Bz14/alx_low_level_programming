d7428b0b38a2eee62be94d028ff8160105675a74
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-26 05:59:01 -0700

    Enocode string using rot13.

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,25 +9,19 @@
 char *rot13(char *n)
 {
 	int j, i, len = strlen(n);
-	char enc_1[] = "abcdefghijklmnopqrstuvwxyz";
-	char enc_2[] = "nopqrstuvwxyzabcdefghijklm";
+	char enc_1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char enc_2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (i = 0; i < len; ++i)
 	{
-		if (isalpha(*(n + i)))
-		{
-			for (j = 0; j < 26; j++)
+			for (j = 0; j < 52; j++)
 			{
-				if (*(n + i) == enc_1[j] || *(n + i) == toupper(enc_1[j]))
+				if (*(n + i) == enc_1[j])
 				{
-					if (isupper(*(n + i)))
-						*(n + i) = toupper(enc_2[j]);
-					else
-						*(n + i) = enc_2[j];
+					*(n + i) = enc_2[j];
 					break;
 				}
 			}
-		}
 	}
 	return (n);
 }
