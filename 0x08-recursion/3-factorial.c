1115fecd99f2a77b1873a6141c6a4d10abf3f538
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-28 23:55:10 -0700

    Find factorial of a number recursively.

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+
+/**
+ * factorial - Find the factorial value of a number
+ * @n:an integer number
+ * Return: factorial of a number
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
