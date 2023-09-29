758401ffcfcb32bb421d77d92f04435287313a37
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-09-29 05:03:27 -0700

    Find square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,28 @@
-
+#include "main.h"
 /**
  * _sqrt_recursion - find square root of a number
  * @n: a number
+ * @i: a number
  * Return: an integer value of square root of a number
- *
- * square - helper function
- * @n: an integer
- * @first: start
- * @last: end
  */
-int square(int n, int first, int last)
-{
-	int mid = (first + last) / 2;
-
-	if (mid * mid == n)
-		return (mid);
-	if (first >= last)
-		return (-1);
-	if (mid * mid > n)
-		return (square(n, first, mid - 1));
-	return (square(n, mid + 1, last));
-}
+int square(int n, int i);
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
+	return (square(n, 1));
+}
+/**
+ * square - helper function
+ * @n: a number
+ * @i: an integer
+ * Return: an integer
+ */
+int square(int n, int i)
+{
+	int num = i * i;
+
+	if (num > n)
 		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
-	return (square(n, 1, n));
+	if (num == n)
+		return (i);
+	return (square(n, i + 1));
 }
