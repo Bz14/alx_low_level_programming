02bb49de6f10e8097401d878692d07ed29a186f6
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-10-01 22:56:36 -0700

    Print the name of the file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print the name of the file
+ * @argc: number of argument
+ * @argv: array of pointers
+ * Return: Always success (0)
+ */
+int main(int argc, char* argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
