23aa8413761fd58c2eeeeb61a59ab3fa334ea485
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-10-03 00:07:54 -0700

    Allocate memory for two dimensional array using malloc.

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+/**
+ * alloc_grid - a pointer to a two dimensional array
+ * @width: width
+ * @height: height
+ * Return: a two dimensional pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **st;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	st = (int **) malloc(sizeof(int *) * width);
+	if (st == NULL)
+		return (NULL);
+	for (i = 0; i < width; i++)
+	{
+		st[i] = malloc(sizeof(int) * height);
+	}
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+		st[i][j] = 0;
+		}
+	}
+	return (st);
+}
