053b869fa6faca0f10cbe5d203255f8b56b3a2b0
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-10-02 22:52:48 -0700

    Duplicate a string.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - pointer to the copy of string
+ * @str: pointer to string
+ * Return: a pointer to newly allocated space
+ */
+char *_strdup(char *str)
+{
+	char *st;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	st = malloc(sizeof(str));
+	if (st == NULL)
+		return (NULL);
+	while (*(str + i) != '\0')
+	{
+		st[i] = *(str + i);
+		i++;
+	}
+	s[i] = '\0';
+	return (st);
+}
