0364833fdaf9e37f870afb52ccf3414b4ab10011
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-10-11 06:03:12 -0700

    Main function pointer.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,29 +10,27 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2, res = 0;
-	char c;
-	int (*func)(int, int);
+	char *c;
 
 	if (argc < 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	c = argv[2][0];
-	if (c != '+' && c != '-' && c != '*' && c != '/' && c != '%')
+	c = argv[2];
+	if (*c != '+' && *c != '-' && *c != '*' && *c != '/' && *c != '%')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	if ((c == '/' || c == '%') && num2 == 0)
+	if ((*c == '/' || *c == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	func = get_op_func(argv[2]);
-	res = func(num1, num2);
+	res = get_op_func(c)(num1, num2);
 	printf("%d\n", res);
 	return (0);
 }
