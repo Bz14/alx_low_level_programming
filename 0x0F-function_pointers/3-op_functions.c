e30b61e614ff04366218293c04bf47acda62f07b
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-10-11 05:43:00 -0700

    Operator functions.

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add - add two number
+ * @a: first number
+ * @b: second number
+ * Return: an integer sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub -subtract two numbers
+ * @a: first number
+ * @b: second number
+ * Return: an integer difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply two numbers
+ * @a: first number
+ * @b: second number
+ * Return: an integer product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide two numbers
+ * @a: first number
+ * @b: second number
+ * Return: an integer quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modules of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: an integer remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
