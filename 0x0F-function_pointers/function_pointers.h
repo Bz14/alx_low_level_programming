c3e816f86c68263b299756b5b955617c62fe065d
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-10-11 02:41:46 -0700

    Header file updated.

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
+#include <stddef.h>
 #ifndef PUTCHAR_FUNCTION
 #define PUTCHAR_FUNCTION
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
-typedef int size_t;
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
