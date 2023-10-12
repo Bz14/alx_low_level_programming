4591a433ee6e68a6684c88d0f4cd2bae31ffc9cb
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-10-12 11:14:32 -0700

    Print vardiac parameters.

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - print numbers
+ * @separator: separator
+ * @n: number of parameter
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, num;
+	va_list ls;
+
+	va_start(ls, n);
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(ls, int);
+		printf("%d", num);
+		if (separator != NULL && i != n - 1)
+			printf("%c", *separator);
+	}
+	va_end(ls);
+	printf("\n");
+}
