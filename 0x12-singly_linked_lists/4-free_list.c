9db019e0808fcda9d98f6966c0e9fdd1cf41549d
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-10-19 12:13:40 -0700

    Free linked lists.

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_list - frees a linked list
+ * @head: a linked list
+ */
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		free(head->str);
+		head = head->next;
+	}
+}
