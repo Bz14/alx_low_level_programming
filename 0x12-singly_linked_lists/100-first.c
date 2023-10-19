31ffaa011990d5367544960df81b807833ebfe65
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-10-19 12:35:37 -0700

    Print before main.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print_string - prints a string
+ */
+void print_string()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+/**
+ * main - entry point
+ * Return: Always success(0)
+ */
+int main(void)
+{
+	print_string();
+	return(0);
+}
