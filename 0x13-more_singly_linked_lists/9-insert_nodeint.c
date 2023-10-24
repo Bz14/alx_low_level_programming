db2f567c6b674920ca10f57a11cf684797d762d5
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-10-24 02:41:19 -0700

    Insert node at index.

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - add node at index
+ * @head:  singly linked list
+ * @idx: index to be searched
+ * @n: data of the node
+ * Return: address of the new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *node, *current;
+	unsigned int count = 0;
+
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	node->next = NULL;
+	if (idx == 0)
+	{
+		node->next = *head;
+		*head = node;
+	}
+	current = *head;
+	while (count < idx - 1)
+	{
+		if (*head == NULL)
+			return (NULL);
+		current = current->next;
+		count++;
+	}
+	if (current == NULL || current->next == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->next = current->next;
+	current->next = node;
+	return (node);
+}
