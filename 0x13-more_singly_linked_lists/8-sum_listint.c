97f98cea455ba4b1c99df83f848a7b1445333566
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-10-24 02:13:36 -0700

    Sum of data's in linked list.

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_listint - sum of all data in nodes
+ * @head:  list of nodes
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
