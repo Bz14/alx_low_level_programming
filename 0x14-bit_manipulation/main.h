a7c6a8650389b5c4ba3dae52e0dad30109133bb2
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-10-26 01:24:32 -0700

    Header file updated.

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,8 @@
 #ifndef BIT_MANIPULATION
 #define BIT_MANIPULATION
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
