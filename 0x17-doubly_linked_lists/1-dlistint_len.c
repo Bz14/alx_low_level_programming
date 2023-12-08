47f7ab070ce21516302566da96b66cd384843e1b
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-12-08 11:19:37 -0800

    Count the number of nodes in doubly linked list.

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - number of elements in a doubly linked list
+ * @h: doubly linked list
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
