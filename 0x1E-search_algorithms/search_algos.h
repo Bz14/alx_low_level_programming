0ec281f43d2c892b6969ec821cf715bfd9a55fbc
Bz14 <eyerusalembezu15@gmail.com>
2024-07-21 15:54:14 +0000

    Header file.

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,19 @@
+#ifndef _SORTING_ALGOS_H_
+#define _SORTING_ALGOS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for ALX project
+ */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
