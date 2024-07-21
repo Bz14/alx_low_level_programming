a865ce7face3986bfcb88c7982e269676eeb5957
Bz14 <eyerusalembezu15@gmail.com>
2024-07-21 16:01:11 +0000

    File updated.

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,16 +4,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
- * struct listint_s - singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- * for ALX project
- */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 #endif
